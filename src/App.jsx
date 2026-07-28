94c501ae9255419a1276b42915ae8f68829b02d9
ktg17 <ktg7studio@gmail.com>
2026-07-28 13:41:17 +0530

    add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import Home from './pages/Home.jsx'
+import Apology from './pages/Apology.jsx'
+import Understanding from './pages/Understanding.jsx'
+import GoingForward from './pages/GoingForward.jsx'
+import MessagePage from './pages/MessagePage.jsx'
+import MusicPlayer from './components/MusicPlayer.jsx'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
+export default function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <MusicPlayer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/apology" element={<Apology />} />
+        <Route path="/understanding" element={<Understanding />} />
+        <Route path="/forward" element={<GoingForward />} />
+        <Route path="/message" element={<MessagePage />} />
+      </Routes>
+    </>
+  )
+}
